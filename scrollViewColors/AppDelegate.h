872adf0eba2cc79e8cfadfb98a9b4e1d4dc9d8fe
paulkoka <pavelkoka@gmail.com>
2018-07-03 14:23:18 +0300

    Initial Commit

---
+++ scrollViewColors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollViewColors
+//
+//  Created by paul on 7/3/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollViewColors/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  scrollViewColors
+//
+//  Created by paul on 7/3/18.
+//  Copyright © 2018 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
