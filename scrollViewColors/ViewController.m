5a259691644236ceb80c20f27a7da8657b7bb9e8
paulkoka <pavelkoka@gmail.com>
2018-07-04 13:19:24 +0300

    done

--- scrollViewColors/ViewController.m
+++ scrollViewColors/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -16,14 +18,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.scrollView.delegate = self;
+    self.view.backgroundColor = UIColor.yellowColor;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void) scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    if ([self.view.backgroundColor isEqual:UIColor.yellowColor]) {
+        self.view.backgroundColor = UIColor.redColor;
+    } else if ([self.view.backgroundColor isEqual:UIColor.redColor]){
+        self.view.backgroundColor = UIColor.yellowColor;
+    }
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 
 @end
